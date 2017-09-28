c74b77011e8a455c3794528322a27ad1573568a0
Aayush Kumar Thakur <aayush.thakur@springernature.com>
2017-09-28 15:27:02 +0530

    Added animation for hamburger

--- js/header.js
+++ js/header.js
@@ -9,15 +9,20 @@ $("#close").on("click",function(){
 })
 
 $(".nav-toggle").on("click",function(){
+    $(".nav-toggle span").toggleClass("close");
     if($("#main-nav").css("display") != "none"){
         $("#main-nav").removeClass("mobile-view");
         $("#main-nav").hide();
-        $(".logo:first").css("width","40%");
+        if($(window).height() > 670){
+            $(".logo:first").css("width","40%");
+        }
         $(".main-content").show();
     }
     else{
         $("#main-nav").addClass("mobile-view");
-        $(".logo:first").css("width","90%");
+        if($(window).height() > 670){
+            $(".logo:first").css("width","90%");
+        }
         $("#main-nav").css("height",($(window).height()-100));
         $("#main-nav").show();
         $(".main-content").hide();
