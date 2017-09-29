b501471aa7d13e7edaef5e987beb3da71e4a45a2
Aayush Kumar Thakur <aayush.thakur@springernature.com>
2017-09-29 15:47:10 +0530

    bUG FIXES FOR HEADER RESPONSIVENESS

--- js/header.js
+++ js/header.js
@@ -13,17 +13,21 @@ $(".nav-toggle").on("click",function(){
     if($("#main-nav").css("display") != "none"){
         $("#main-nav").removeClass("mobile-view");
         $("#main-nav").hide();
-        if($(window).height() > 670){
+        if($(window).height() > 750){
             $(".logo:first").css("width","40%");
         }
+        // $(".logo:first").css("width","40%");
         $(".main-content").show();
     }
     else{
         $("#main-nav").addClass("mobile-view");
-        if($(window).height() > 670){
+        if($(window).height() < 750){
+            $(".logo:first").css("width","100%");
+        }
+        else{
             $(".logo:first").css("width","90%");
         }
-        $("#main-nav").css("height",($(window).height()-100));
+        $("#main-nav").css("height",($(window).height()));
         $("#main-nav").show();
         $(".main-content").hide();
     }
