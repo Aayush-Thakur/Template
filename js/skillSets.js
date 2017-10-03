fd7e4d7cad5fad7c9033d51b62161b3b812613f2
Aayush Kumar Thakur <aayush.thakur@springernature.com>
2017-10-03 17:54:31 +0530

    Animation for skill bar improved.

--- js/skillSets.js
+++ js/skillSets.js
@@ -3,8 +3,8 @@ $(window).on("load",function(){
     skillBar.append("<div></div>") ;
     var skillsets = $(".skillsets .black-background div");
     skillsets.addClass("skillset-bar");
-    $("#design").children(".skillset-bar").animate({width:"90%"},3000);
-    $("#prototyping").children(".skillset-bar").animate({width:"80%"},3000);
-    $("#basic").children(".skillset-bar").animate({width:"95%"},3000);
-    $("#visual-design").children(".skillset-bar").animate({width:"90%"},3000);
+    $("#design").children(".skillset-bar").animate({width:"90%"},1500);
+    $("#prototyping").children(".skillset-bar").animate({width:"80%"},1500);
+    $("#basic").children(".skillset-bar").animate({width:"95%"},1500);
+    $("#visual-design").children(".skillset-bar").animate({width:"90%"},1500);
 })
