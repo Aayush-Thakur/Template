8f7bff1cd3a37610c0bbb95ea346ed7558c06d5b
Aayush Kumar Thakur <aayush.thakur@springernature.com>
2017-10-04 17:36:19 +0530

    Added responsiveness to remaining sections of page.

--- js/skillSets.js
+++ js/skillSets.js
@@ -3,8 +3,8 @@ $(window).on("load",function(){
     skillBar.append("<div></div>") ;
     var skillsets = $(".skillsets .black-background div");
     skillsets.addClass("skillset-bar");
-    $("#design").children(".skillset-bar").animate({width:"90%"},1500);
-    $("#prototyping").children(".skillset-bar").animate({width:"80%"},1500);
-    $("#basic").children(".skillset-bar").animate({width:"95%"},1500);
-    $("#visual-design").children(".skillset-bar").animate({width:"90%"},1500);
+    $("#design").children(".skillset-bar").animate({width:"90%"},2000);
+    $("#prototyping").children(".skillset-bar").animate({width:"80%"},2000);
+    $("#basic").children(".skillset-bar").animate({width:"95%"},2000);
+    $("#visual-design").children(".skillset-bar").animate({width:"90%"},2000);
 })
