4c371bee42a7b50d1476d44ba0aca54e140bfb98
Aayush Kumar Thakur <aayush.thakur@springernature.com>
2017-10-05 11:49:34 +0530

    Improved responsiveness, alignments of headers and hover effects of links.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 define(function(require) {
-    var header = require("./header");
-    var carousel = require("./carousel");
-    var skillsets = require("./skillSets");
+    var header = require("./header");               //All header animations and onclicks
+    var carousel = require("./carousel");           //Carousel
+    var skillsets = require("./skillSets");         //Animation for Skills bar
 });
