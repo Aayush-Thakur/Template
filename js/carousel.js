e74d3cd267210b448b489d2b9e87fa6b58e2cfe1
Aayush Kumar Thakur <aayush.thakur@springernature.com>
2017-10-06 12:21:33 +0530

    Carousel bug removed.

--- js/carousel.js
+++ js/carousel.js
@@ -3,6 +3,10 @@ $(document).ready(function(){
     var carouselWidth = carousel.innerWidth();
     var sliderCount = carousel.children().length;
     var width = carouselWidth / sliderCount ;
+    $(window).on("resize",function(){
+        carouselWidth = carousel.innerWidth();
+        width = carouselWidth / sliderCount ;
+    })
     setInterval(function(){
         carousel.animate({marginLeft:-width},1000,function(){
             $(this).find("li:last").after($(this).find("li:first"));
