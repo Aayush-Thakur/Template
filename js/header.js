3bb74cd8d8dcc2cbdfaad068b42ed7e1828e0b05
Aayush Kumar Thakur <aayush.thakur@springernature.com>
2017-10-06 14:10:52 +0530

    Fixed some of the issue of interactions through jquery.

--- js/header.js
+++ js/header.js
@@ -1,10 +1,12 @@
 $("#search-icon").on("click",function(){
     $("#search-icon-wrapper").hide();
+    $("#main-nav").hide();
     $("#search").show();
 })
 
 $("#close").on("click",function(){
     $("#search-icon-wrapper").show();
+    $("#main-nav").show();
     $("#search").hide();
 })
 
@@ -14,21 +16,18 @@ $(".nav-toggle").on("click",function(){
         $("#main-nav").removeClass("mobile-view");
         $("#main-nav").hide();
         if(screen.width > 600){
-            $(".logo:first").css("width","40%");
+            $(".header-nav-wrapper").removeClass("cover");
         }
         $(".main-content").show();
         $("footer").show();
     }
     else{
         $("#main-nav").addClass("mobile-view");
-        if(screen.width < 600){
-            $(".logo:first").css("width","100%");
-        }
-        else{
-            $(".logo:first").css("width","90%");
-        }
         $("#main-nav").show();
         $("#main-nav").css("height",screen.height);
+        if(screen.width > 600){
+            $(".header-nav-wrapper").addClass("cover");
+        }
         $(".main-content").hide();
         $("footer").hide();
     }
